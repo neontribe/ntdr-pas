726fdbd84bd1acf7424dcc336ff953e93a210735
Ruby <rubydferguson@gmail.com>
2015-07-02 16:08:47 +0100

    rewrote createchangelog in python

---
+++ library/ntdr_slacken_local_permissions.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+# Find changelog vesion
+
+import sys, os, json, shlex
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            path = dict(required=True),
+            version = dict(default='no', type='bool'),
+        ),
+        supports_check_mode = True
+    )
+
+    path = module.params.get('path')
+    path = os.path.expanduser(path)
+
+    if not os.path.isfile(path+'/sites/default/settings.php'): 
+        module.fail_json(msg="Drupal not found")
+    else:
+        os.chmod(path, 0775)
+        os.chmod(path,0664)
+        #os.chown(path, uid, gid)
+        d = {
+            'test':"successful",
+        }
+
+        module.exit_json(changed=False, stat=d)
+
+# import module snippets
+from ansible.module_utils.basic import *
+
+main()
