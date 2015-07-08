8e095206858f285f9286e049c7223f3e8cd7451a
Adam <adamdanferguson@gmail.com>
2015-07-08 14:11:44 +0100

    finished

--- playbooks/library/ntdr_slacken_local_permissions.py
+++ playbooks/library/ntdr_slacken_local_permissions.py
@@ -2,7 +2,13 @@
 
 # Find changelog vesion
 
-import sys, os, json, shlex, glob, subprocess
+import sys
+import os
+import json
+import shlex
+import glob
+import subprocess
+from pwd import getpwnam
 
 
 def get_sudo_info():
@@ -15,6 +21,7 @@ def get_sudo_info():
         element = element.split('=')
         if len(element) >=2:
 	    sudo_enviroment[element[0]] = element[1]
+
     return sudo_enviroment
 
 
@@ -25,48 +32,50 @@ def recursive_file_permissions(path,uid=-1,gid=-1):
      UID and GID default to -1, and mode is required
     '''
     for item in glob.glob(path+'/*'):
+        item = item.split(os.sep)[-1]
         if os.path.isdir(item):
-	    os.chmod(os.path.join(path,item),0775)
-            recursive_file_permissions(os.path.join(path,item),uid,gid)
+	    os.chmod(path+item,0775) #0775
+            recursive_file_permissions(path + item,uid,gid)
             os.chown(path,uid, gid)
         else:
             try:
-                os.chmod(os.path.join(path,item),0664)
+                os.chmod(path + item,0664) #0664 
 		os.chown(path,uid,gid)
             except:
                 print 'error'
-    	
+           	
     files = []
-    start_dir = path
-    pattern   = "/sites/*/files"
+    start_dir = path+'/sites/'
+    pattern   = "*/files"
     for dir,_,_ in os.walk(start_dir):
         files.extend(glob.glob(os.path.join(dir,pattern)))
 
     for element in files:
-    	os.chown(element,'www-data','www-data')
-
+    	os.chown(element, getpwnam('www-data').pw_uid, getpwnam('www-data').pw_gid)
     
+
+
+
+    os.chmod(path+'/sites/default',0755) #0755
+
+   
+
+      
     files = []
-    start_dir = path
-    pattern   = "/sites/default"
+    start_dir = path+'/sites/default/'
+    pattern   = "*.php"
+
     for dir,_,_ in os.walk(start_dir):
         files.extend(glob.glob(os.path.join(dir,pattern)))
     
     for element in files:
-        os.chmod(element,0755)
+        os.chmod(element,0644) #0644
     
 
-    
-    files = []
-    start_dir = path
-    pattern   = "/sites/default/*.php"
-    for dir,_,_ in os.walk(start_dir):
-        files.extend(glob.glob(os.path.join(dir,pattern)))
-    
-    for element in files:
-        os.chmod(element,0644)
-    
     return files
+
+
+
 
 def main():
     module = AnsibleModule(
@@ -78,16 +87,17 @@ def main():
     )
     sudo_info = get_sudo_info()
     path = module.params.get('path')
-    path = os.path.expanduser(path)
-
+    fh = open('printout','w')
+    fh.write(path)
+    fh.close()
     if not os.path.isfile(path+'/sites/default/settings.php'):
 	failure_message = 'failure but: '+path+'/sites/default/settings.php' 
         module.fail_json(msg=failure_message)
     else:
-	recursive_file_permissions(path,int(sudo_info['SUDO_UID']),int(sudo_info['SUDO_GID']))
+	return_path = recursive_file_permissions(path,int(sudo_info['SUDO_UID']),int(sudo_info['SUDO_GID']))
 	
     d = {
-        'status':'done'
+        'status':return_path
     }
 
     module.exit_json(changed=False, stat=d)
